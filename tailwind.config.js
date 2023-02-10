9fb7e9801fcd6a712524da4760d9cb78a7b40df3
mamun8920 <a.mamun8920@gmail.com>
2023-02-11 02:29:55 +0600

    food lover

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#fc1f15',
+        secondary: {
+          100: '#8d98ff',
+          200: '#636bff',
+        }
+      },
+      fontFamily: {
+        'body': ['Montserrat']
+      }
+
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
